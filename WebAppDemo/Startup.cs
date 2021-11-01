55bc3f7e36fea6a76cec9804806d758c0c854862
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-11-01 11:23:24 +0100

    MVC with 2 views

--- WebAppDemo/Startup.cs
+++ WebAppDemo/Startup.cs
@@ -16,6 +16,7 @@ namespace WebAppDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,17 +27,17 @@ namespace WebAppDemo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
